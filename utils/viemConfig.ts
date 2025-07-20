dad210dac452c8537294224f8af53fe3c196113f
yashgoel <yash.goel8370@gmail.com>
2025-07-20 20:46:48 +0530

    Switched to Polygon Amoy

--- utils/viemConfig.ts
+++ utils/viemConfig.ts
@@ -3,5 +3,5 @@ import { polygonAmoy } from 'viem/chains'
 
 export const publicClient = createPublicClient({ 
   chain: polygonAmoy,
-  transport: http('https://eth-sepolia.g.alchemy.com/v2/lsabosOMapNRPIc7cq3_V')
+  transport: http('https://polygon-amoy.g.alchemy.com/v2/lsabosOMapNRPIc7cq3_V')
 })
