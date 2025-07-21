11e0d6579b48c3ca5af0062c96ac7dc2b8b673f4
yashgoel <yash.goel8370@gmail.com>
2025-07-21 16:11:57 +0530

    Changed RPC URL to Infura

--- utils/viemConfig.ts
+++ utils/viemConfig.ts
@@ -3,5 +3,5 @@ import { polygonAmoy } from 'viem/chains'
 
 export const publicClient = createPublicClient({ 
   chain: polygonAmoy,
-  transport: http('https://polygon-amoy.infura.io/v3/37a6105087cc441dae7833b5a0f2ebe2')
+  transport: http('https://polygon-amoy.infura.io/v3/0143df0befda47cb8c39faca907961e3')
 })
