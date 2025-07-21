eaa66e5c281a837f49b612586dfe85ee6dde7939
yashgoel <yash.goel8370@gmail.com>
2025-07-21 17:58:02 +0530

    commit

--- utils/viemConfig.ts
+++ utils/viemConfig.ts
@@ -3,5 +3,5 @@ import { polygonAmoy } from 'viem/chains'
 
 export const publicClient = createPublicClient({ 
   chain: polygonAmoy,
-  transport: http('https://polygon-amoy.g.alchemy.com/v2/lsabosOMapNRPIc7cq3_V')
+  transport: http('https://polygon-amoy.infura.io/v3/652ee568eab9493b9111ff9f583ab35e')
 })
